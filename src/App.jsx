906d0a257ab5cd9563aae99e9ebc4739fbc1a660
Brian Wu <wubrian1234@hotmail.com>
2019-01-08 21:06:49 +0000

    break down html components to indv. files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+        <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <ChatBar/>
+        <MessageList/>
+      </div>
     );
   }
 }
---
+++ src/MessageList.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+import Notification from './Notification.jsx';
+
+class MessageList extends Component {
+    render() {
+      return (
+        <main className="messages">
+        <Message/>
+        <Notification/>
+      </main>
+      );
+    }
+  }
+
+export default MessageList; 
